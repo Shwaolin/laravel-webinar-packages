a6bb9ef98c1ee7cd7ff2d042b72dc01186120957
Dorian Bartsch <dorian@shwao.com>
2021-01-20 11:36:40 +0100

    Fix formula

--- src/Temperature.php
+++ src/Temperature.php
@@ -4,15 +4,14 @@ namespace Shwaolin\LaravelWebinarPackages;
 
 class Temperature
 {
-    protected float $celsius;
+    public float $celsius;
 
-    public function __construct(float $celsius)
-    {
+    public function __construct(float $celsius) {
         $this->celsius = $celsius;
     }
 
     public function toFahrenheit(): float
     {
-        return ($this->celsius * 1.8) + 3;
+        return ($this->celsius * 1.8) + 32;
     }
 }
